aeb364cd09893b62cc2ed5181cdddc82f5cbb139
hseager <harvey.seager@googlemail.com>
2024-02-03 07:20:30 +0000

    Add viewport meta

--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-<title><?php wp_title('-',true, 'right'); ?><?php bloginfo('name'); ?></title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<title><?php wp_title('-',true, 'right'); ?><?php bloginfo('name'); ?></title>
 
-<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body>
